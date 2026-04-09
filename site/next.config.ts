1cee3c102a7bb87581d70587c6d8c8ad96bae723
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2026-04-09 10:45:09 -0400

    Add basePath support for GitHub Pages subpath deployment

--- site/next.config.ts
+++ site/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+  ...(process.env.BASE_PATH ? { basePath: process.env.BASE_PATH } : {}),
 };
 
 export default nextConfig;
